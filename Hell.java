82fd9996ef3ad0a7754a0a1fef6c4bb46340f3e8
loganathannn <loguhari09@gmail.com>
2017-07-05 11:54:52 +0530

    Create Hell.java

---
+++ Hell.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Hell
+{
+  public static void main(String[] args)
+  {
+    Scanner sc=new Scanner(System.in); 
+    String b=sc.nextLine();
+    int a=5;
+    for(int i=1
+    ;i<=a;i++)
+    {
+      System.out.println(b);  
+    }
+    
+  }
+}
