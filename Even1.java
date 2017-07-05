490c7f94c138ec9d558bff1f64eaa39803c6a157
loganathannn <loguhari09@gmail.com>
2017-07-05 14:04:08 +0530

    Create Even1.java

---
+++ Even1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Even1
+{
+  public static void main(String[] args)
+  {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int m=sc.nextInt();
+    //int a=sc.nextInt();
+    for(int i=n;i<m;i++)
+    {
+      if(i%2==0)
+      
+      {
+        System.out.println(i);
+      } 
+    } 
+    
+    
+  }
+}
