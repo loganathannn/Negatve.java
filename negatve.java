3dec6d32dee19d5ec374dbb81454bf3ef41a5303
loganathannn <loguhari09@gmail.com>
2017-07-05 10:37:22 +0530

    Create negatve.java

---
+++ negatve.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Negatve
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextLine();
+if(n>0)
+{
+System.out.println("positive");
+}
+else
+System.out.println("negative");
+}
+}
