a666c7fbf894551c309965245d4f3866652789b0
loganathannn <loguhari09@gmail.com>
2017-07-18 14:11:22 +0530

    Create Dupp.java

---
+++ Dupp.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class Dupp {
+public static void main(String args[])throws IOException{
+	char c;
+	String ans="";
+	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	System.out.print("uujxch");
+	String s=br.readLine();
+	int l=s.length();
+	for(int i=0;i<l;i++)
+	{
+	c=s.charAt(i);
+	if(c!=' ')
+	ans=ans+c;
+	s=s.replace(c,' ');
+	}
+	System.out.println(ans);
+	}
+	}
+
+
