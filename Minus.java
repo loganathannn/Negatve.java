9206e1c5c0effdc9151ce9476be277d476bf62c8
loganathannn <loguhari09@gmail.com>
2017-08-01 14:17:19 +0530

    Create Minus.java

---
+++ Minus.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Minus {
+public static void main(String[] args)
+{
+	int max=0;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter the value");
+	int n=sc.nextInt();
+	int[] a=new int[n];
+	int[] p=new int[n];
+	for(int i=0;i<n;i++)
+	{
+		a[i]=sc.nextInt();
+	}
+	for(int j=0;j<n-1;j++)
+	{
+		int k=Math.abs(a[j]-a[j+1]);
+		p[j]=k;
+	}
+		max=p[0];
+for(int i = 0;i<n;i++)
+{
+	if(p[i]>max)
+	{
+		max=p[i];
+	}}
+
+System.out.println(max);
+}
+}
